1e7488b4e823a1799950fcc430993a95ebad0f1b
Allan Sun <allan.sun@bricre.com>
2021-09-02 01:01:54 +0100

    Init commit

---
+++ src/ResponseTypes.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Ebay\Sell\Recommendation;
+
+use OpenAPI\Runtime\ResponseTypes as AbstractResponseTypes;
+
+class ResponseTypes extends AbstractResponseTypes
+{
+    public static $types = [
+    ];
+}
